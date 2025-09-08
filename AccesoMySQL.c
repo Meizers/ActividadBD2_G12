82df9e0d038b448a9eea850b22d0585f2d7436ab
Tomas Resnik <101259872+Meizers@users.noreply.github.com>
2025-09-07 21:24:53 -0300

    mogolico

--- AccesoMySQL.c
+++ AccesoMySQL.c
@@ -6,6 +6,62 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+MYSQL *conectarMySQL(const char *remote_host,
+                     const char *remote_port,
+                     const char *username,
+                     const char *userpassword,
+                     char databasename[])
+{
+    MYSQL *conn;
+    const char *server = remote_host;
+    const char *user = username;
+    const char *password = userpassword;
+    const char *database = databasename;
+
+    conn = mysql_init(NULL);
+    if (conn == NULL){
+        printf("Error: mysql_init! \n");
+        return NULL;
+    }
+    else {
+        char respuesta[1024] = "";
+        if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)){
+            sprintf(respuesta, "%s\n", mysql_error(conn));
+            return NULL;
+        }
+    }
+    return conn;
+}
+
+void querySQL(MYSQL *conn, const char *query)
+{
+    char respuesta[1024] = "";
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    if (mysql_query(conn, query)){
+        printf(respuesta, "%s\n", mysql_error(conn));
+    } else {
+        res = mysql_use_result(conn);
+        int num_attrib = mysql_num_fields(res);
+        while ((row = mysql_fetch_row(res)) != NULL){
+            int i;
+            for (i=0; i< num_attrib; i++){
+                strcat(respuesta, row[i]);
+                strcat(respuesta, " ");
+            }
+            strcat(respuesta, "\n");
+        }
+        strcat(respuesta, "\0");
+        mysql_free_result(res);
+    }
+}
+
+void cerrarSesionSQL(MYSQL *conn)
+{
+    mysql_close(conn);
+}
+
+/*
 void funcionMysql(const char *remote_host, 
     const char *remote_port, 
     const char *username, 
@@ -52,3 +108,4 @@ void funcionMysql(const char *remote_host,
         mysql_close(conn);
     }
 }
+*/
